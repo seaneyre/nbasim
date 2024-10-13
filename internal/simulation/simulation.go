836d62a9786cdbea35ee7d69f3a0d66f8ad2949b
Sean <eryenaes@gmail.com>
2024-10-12 22:06:34 -0400

    prepare array of events and calculate the game clock time

--- internal/simulation/simulation.go
+++ internal/simulation/simulation.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/seaneyre/nbasim/internal/retrieve"
 	"encoding/json"
+	"strconv"
 )
 
 type Simulation struct {
-	nba_game_id int
+	nba_game_id string
 	time_factor float64
 }
 
-func New(nbaGameID int) *Simulation {
+func New(nbaGameID string) *Simulation {
 	return &Simulation{
 		nba_game_id: nbaGameID,
 		time_factor: 1.0,
@@ -21,23 +22,50 @@ func New(nbaGameID int) *Simulation {
 
 func (s *Simulation) Run() error {
 	fmt.Println("Running simulation")
-	fmt.Printf("NBA Game ID: %d\n", s.nba_game_id)
+	fmt.Printf("NBA Game ID: %s\n", s.nba_game_id)
 	fmt.Printf("Time factor: %f\n", s.time_factor)
 	
-	url := "https://cdn.nba.com/static/json/liveData/playbyplay/playbyplay_0022000180.json"
-	fmt.Println("Fetching events from:", url)
-
-	j, err := retrieve.FetchResponseFromURL(url)
+	
+	resp, err := retrieve.GetPlayByPlayResponse(s.nba_game_id)
 	if err != nil {
 		return err
 	}
 
-	var events retrieve.PlayByPlayResponse
-	if err := json.Unmarshal(j, &events); err != nil {
-		fmt.Println("Can not unmarshal JSON")
-	}
-	str, _ := json.MarshalIndent(events.Game.Actions[0], "", "  ")
+	str, _ := json.MarshalIndent(resp.Game.Actions[0], "", "  ")
 	fmt.Println(string(str))
 
+	events := PrepareEvents(resp)
+	fmt.Println(events[0])
+
 	return nil
+}
+
+func PrepareEvents(resp retrieve.PlayByPlayResponse) []Event {
+	var events []Event
+	for _, action := range resp.Game.Actions {
+		game_clock_time, err := GetGameClockTime(action.Clock, action.Period)
+		if err != nil {
+			fmt.Printf("Error getting Game Clock Time from clock=%s; period=%d", action.Clock, action.Period)
+		}
+		// fmt.Printf("clock=%s; period=%d; game_clock_time=%d\n", action.Clock, action.Period, game_clock_time)
+
+		event := Event{
+			GameClockTime: game_clock_time,
+			ActionType: action.ActionType,
+			Action: action,
+		}
+		events = append(events, event)
+	}
+	return events
+}
+
+func GetGameClockTime(clock_string string, period int) (int, error) {
+	// TODO write tests for this!
+	minutes, err := strconv.Atoi(clock_string[2:4])
+	seconds, err := strconv.Atoi(clock_string[5:7])
+	if err != nil {
+		return 0, err
+	}
+	game_clock_time := ((12 * (period - 1)) * 60) + ((12 - minutes) * 60) + (60 - seconds) - 60
+	return game_clock_time, nil
 }
