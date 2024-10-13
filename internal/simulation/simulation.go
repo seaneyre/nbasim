cfa0e4fb61d9a21e6b559a9ea084bb8c8ff75f9d
Sean <eryenaes@gmail.com>
2024-10-12 23:32:12 -0400

    wait for game start

--- internal/simulation/simulation.go
+++ internal/simulation/simulation.go
@@ -3,9 +3,10 @@ package simulation
 import (
 	_ "fmt"
 	"os"
+	"sort"
 	"time"
 
-	"encoding/json"
+	_ "encoding/json"
 	"strconv"
 
 	"github.com/rs/zerolog"
@@ -17,6 +18,7 @@ type Simulation struct {
 	nba_game_id string
 	time_factor float64
 	real_start_time time.Time
+	simulated_game_clock_time int
 }
 
 func New(nbaGameID string, realStartTime time.Time) *Simulation {
@@ -24,6 +26,7 @@ func New(nbaGameID string, realStartTime time.Time) *Simulation {
 		nba_game_id: nbaGameID,
 		time_factor: 1.0,
 		real_start_time: realStartTime,
+		simulated_game_clock_time: 0,
 	}
 }
 
@@ -43,12 +46,35 @@ func (s *Simulation) Run() error {
 		return err
 	}
 
-	str, _ := json.MarshalIndent(resp.Game.Actions[0], "", "  ")
-	log.Print(string(str))
+	// str, _ := json.MarshalIndent(resp.Game.Actions[0], "", "  ")
+	// log.Print(string(str))
 
 	log.Info().Msg("Preparing list of events from Play-By-Play response")
 	events := PrepareEvents(resp)
 	log.Info().Msgf("%d events prepared", len(events))
+
+	log.Info().Msgf("now=%s", time.Now().Format(time.RFC3339))
+	log.Info().Msgf("real_start_time=%s", s.real_start_time.Format(time.RFC3339))
+	game_should_have_started := time.Now().Compare(s.real_start_time)
+	log.Info().Msgf("game_should_have_started=%d", game_should_have_started)
+
+	log.Info().Msg("Waiting for game start...")
+	for time.Now().Compare(s.real_start_time) == -1 {
+		time_to_go := s.real_start_time.Sub(time.Now())
+		log.Info().Msgf("%f seconds until game starts", time_to_go.Seconds())
+		sleep_duration := time_to_go.Seconds()
+		if sleep_duration > 0 {
+			log.Info().Msgf("Sleeping for %f seconds", sleep_duration)
+			time.Sleep(time.Duration(sleep_duration * float64(time.Second)))
+		}
+	}
+
+	log.Info().Msg("Starting game")
+	// for _, event := range events {
+		
+	// 	event.GameClockTime
+	// } 
+
 
 	return nil
 }
@@ -60,8 +86,6 @@ func PrepareEvents(resp retrieve.PlayByPlayResponse) []Event {
 		if err != nil {
 			log.Printf("Error getting Game Clock Time from clock=%s; period=%d", action.Clock, action.Period)
 		}
-		// log.Printf("clock=%s; period=%d; game_clock_time=%d\n", action.Clock, action.Period, game_clock_time)
-
 		event := Event{
 			GameClockTime: game_clock_time,
 			ActionType: action.ActionType,
@@ -69,6 +93,9 @@ func PrepareEvents(resp retrieve.PlayByPlayResponse) []Event {
 		}
 		events = append(events, event)
 	}
+	sort.Slice(events, func(i, j int) bool {
+		return events[i].GameClockTime < events[j].GameClockTime
+	})
 	return events
 }
 
