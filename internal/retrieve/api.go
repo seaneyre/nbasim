c00984cdd59e57c21a7c79eaf608f3e3113fc38f
Sean <eryenaes@gmail.com>
2024-10-12 22:05:27 -0400

    move the get function to retreive

--- internal/retrieve/api.go
+++ internal/retrieve/api.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"io"
 	"time"
+
+	"encoding/json"
 )
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
@@ -20,4 +22,20 @@ func FetchResponseFromURL(url string) ([]byte, error) {
 	body, err := io.ReadAll(resp.Body)
 
 	return body, nil
+}
+
+func GetPlayByPlayResponse(nba_game_id string) (PlayByPlayResponse, error) {
+	url := fmt.Sprintf("https://cdn.nba.com/static/json/liveData/playbyplay/playbyplay_%s.json", nba_game_id)
+	fmt.Println("Fetching events from:", url)
+
+	j, err := FetchResponseFromURL(url)
+	if err != nil {
+		return PlayByPlayResponse{}, err
+	}
+
+	var events PlayByPlayResponse
+	if err := json.Unmarshal(j, &events); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+	return events, nil
 }
